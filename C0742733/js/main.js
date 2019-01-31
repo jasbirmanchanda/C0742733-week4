31415a10f27e786ec8297144804b1c42967d235c
Jasbir manchanda <jasbirmanchanda7@gmail.com>
2019-01-31 11:44:19 -0500

    changes

--- C0742733/js/main.js
+++ C0742733/js/main.js
@@ -1,1 +1,4 @@
-﻿// Your code here!
+﻿function func() {
+    var date = new Date()
+    document.body.innerHTML = "<h1> The date is " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + "<h1>"
+}
